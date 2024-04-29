9380b631f4a73270097c62c9d673ec68241267ed
Miroslav Bonchev Bonchev <miroslav.bonchev@proton.me>
2024-04-29 22:53:49 +0300

    Adding gateway and an early test-societal service.

---
+++ _system/ELIAS_Gateway/Program.cs
@@ -0,0 +1,27 @@
+using Yarp.ReverseProxy.Transforms;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddReverseProxy().AddTransforms( context =>
+{
+   context.CopyRequestHeaders = true;
+   context.AddOriginalHost( useOriginal: true );
+   context.UseDefaultForwarders = true;
+} ).LoadFromConfig( builder.Configuration.GetSection( "ReverseProxy" ) );
+
+var app = builder.Build();
+
+if( app.Environment.IsDevelopment() )
+{
+   app.UseSwagger();
+   app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.Run();
